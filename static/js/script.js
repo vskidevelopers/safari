5c2586cd741a6cc31e1e055690efb8b280a29e71
Ben <71015535+vskidevelopers@users.noreply.github.com>
2021-07-16 18:22:10 +0300

    Fixed The nav bars

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,18 @@
 function Notavailable() {
   alert("Function not available currently");
 }
+
+$(document).scroll(function () {
+  if ($(window).scrollTop() >= $("header").offset().top) {
+    $("nav").addClass("sticky");
+    $(".logoImg").addClass("height");
+  } else {
+    $("nav").removeClass("sticky");
+    $(".logoImg").removeClass("height");
+  }
+});
+
+$("body").scrollspy({
+  target: ".navbar",
+  offset: 80,
+});
